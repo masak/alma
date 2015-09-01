cd41bc714d1cb27cfebd0a97d64703f00e854162
Carl Masak <cmasak@gmail.com>
2015-09-01 21:41:51 +0200

    greatly simplify op installation API

--- lib/_007/Parser.pm
+++ lib/_007/Parser.pm
@@ -65,81 +65,37 @@ class OpLevel {
     has @.prepostfixprec;
     has $!prepostfix-boundary = 0;
 
-    method add-prefix($op, $q, :$assoc!) {
-        %!ops<prefix>{$op} = $q;
-        my $prec = Prec.new(:$assoc, :ops{ $op => $q });
-        @!prepostfixprec.splice($!prepostfix-boundary, 0, $prec);
-        $!prepostfix-boundary++;
-    }
+    method install($type, $op, $q?, :%precedence, :$assoc) {
+        %!ops{$type}{$op} = $q !=== Any ?? $q !! {
+            prefix => Q::Prefix::Custom[$op],
+            infix => Q::Infix::Custom[$op],
+            postfix => Q::Postfix::Custom[$op],
+        }{$type};
 
-    method add-prefix-tighter($op, $q, $other-op, :$assoc!) {
-        %!ops<prefix>{$op} = $q;
-        my $pos = @!prepostfixprec.first-index(*.contains($other-op));
-        my $prec = Prec.new(:$assoc, :ops{ $op => $q });
-        @!prepostfixprec.splice($pos + 1, 0, $prec);
-        if $pos <= $!prepostfix-boundary {
-            $!prepostfix-boundary++;
+        my @namespace := $type eq 'infix' ?? @!infixprec !! @!prepostfixprec;
+        sub new-prec() {
+            return Prec.new(:assoc($assoc // "left"), :ops{ $op => $q });
         }
-    }
-
-    method add-prefix-equal($op, $q, $other-op, :$assoc!) {
-        %!ops<prefix>{$op} = $q;
-        my $prec = @!prepostfixprec.first(*.contains($other-op));
-        die X::Associativity::Conflict.new
-            if defined($assoc) && $assoc ne $prec.assoc;
-        $prec.ops{$op} = $q;
-    }
-
-    method add-infix($op, $q, :$assoc!) {
-        %!ops<infix>{$op} = $q;
-        my $prec = Prec.new(:$assoc, :ops{ $op => $q });
-        @!infixprec.push($prec);
-    }
-
-    method add-infix-looser($op, $q, $other-op, :$assoc!) {
-        %!ops<infix>{$op} = $q;
-        my $pos = @!infixprec.first-index(*.contains($other-op));
-        my $prec = Prec.new(:$assoc, :ops{ $op => $q });
-        @!infixprec.splice($pos, 0, $prec);
-    }
-
-    method add-infix-tighter($op, $q, $other-op, :$assoc!) {
-        %!ops<infix>{$op} = $q;
-        my $pos = @!infixprec.first-index(*.contains($other-op));
-        my $prec = Prec.new(:$assoc, :ops{ $op => $q });
-        @!infixprec.splice($pos + 1, 0, $prec);
-    }
-
-    method add-infix-equal($op, $q, $other-op, :$assoc?) {
-        %!ops<infix>{$op} = $q;
-        my $prec = @!infixprec.first(*.contains($other-op));
-        die X::Associativity::Conflict.new
-            if defined($assoc) && $assoc ne $prec.assoc;
-        $prec.ops{$op} = $q;
-    }
-
-    method add-postfix($op, $q, :$assoc!) {
-        %!ops<postfix>{$op} = $q;
-        my $prec = Prec.new(:$assoc, :ops{ $op => $q });
-        @!prepostfixprec.push($prec);
-    }
-
-    method add-postfix-looser($op, $q, $other-op, :$assoc!) {
-        %!ops<postfix>{$op} = $q;
-        my $pos = @!prepostfixprec.first-index(*.contains($other-op));
-        my $prec = Prec.new(:$assoc, :ops{ $op => $q });
-        @!prepostfixprec.splice($pos, 0, $prec);
-        if $pos <= $!prepostfix-boundary {
-            $!prepostfix-boundary++;
+        if %precedence<tighter> || %precedence<looser> -> $other-op {
+            my $pos = @namespace.first-index(*.contains($other-op));
+            $pos += %precedence<tighter> ?? 1 !! 0;
+            @namespace.splice($pos, 0, new-prec());
+            if $type eq 'prefix' | 'postfix' && $pos <= $!prepostfix-boundary {
+                $!prepostfix-boundary++;
+            }
         }
-    }
-
-    method add-postfix-equal($op, $q, $other-op, :$assoc!) {
-        %!ops<postfix>{$op} = $q;
-        my $prec = @!prepostfixprec.first(*.contains($other-op));
-        die X::Associativity::Conflict.new
-            if defined($assoc) && $assoc ne $prec.assoc;
-        $prec.ops{$op} = $q;
+        elsif %precedence<equal> -> $other-op {
+            my $prec = @namespace.first(*.contains($other-op));
+            die X::Associativity::Conflict.new
+                if $assoc !=== Any && $assoc ne $prec.assoc;
+            $prec.ops{$op} = $q;
+        }
+        elsif $type eq 'prefix' {
+            @namespace.splice($!prepostfix-boundary++, 0, new-prec());
+        }
+        else {
+            @namespace.push(new-prec());
+        }
     }
 
     method clone {
@@ -168,12 +124,12 @@ class Parser {
         my $opl = OpLevel.new;
         @!oplevels.push: $opl;
 
-        $opl.add-prefix('-', Q::Prefix::Minus, :assoc<left>);
+        $opl.install('prefix', '-', Q::Prefix::Minus, :assoc<left>);
 
-        $opl.add-infix('=', Q::Infix::Assignment, :assoc<right>);
-        $opl.add-infix('==', Q::Infix::Eq, :assoc<left>);
-        $opl.add-infix('+', Q::Infix::Addition, :assoc<left>);
-        $opl.add-infix-equal('~', Q::Infix::Concat, "+");
+        $opl.install('infix', '=', Q::Infix::Assignment, :assoc<right>);
+        $opl.install('infix', '==', Q::Infix::Eq, :assoc<left>);
+        $opl.install('infix', '+', Q::Infix::Addition, :assoc<left>);
+        $opl.install('infix', '~', Q::Infix::Concat, :precedence{ equal => "+" });
     }
 
     grammar Syntax {
@@ -456,7 +412,14 @@ class Parser {
         }
 
         sub maybe-install-operator($identname, @trait) {
-            my %trait;
+            return
+                unless $identname ~~ / (< prefix infix postfix >)
+                                       ':<' (<-[>]>+) '>' /;
+
+            my $type = ~$0;
+            my $op = ~$1;
+
+            my %precedence;
             my @prec-traits = <equal looser tighter>;
             my $assoc;
             for @trait -> $trait {
@@ -469,10 +432,10 @@ class Parser {
                     sub check-if-op($s) {
                         die "Unknown thing in '$name' trait"
                             unless $s ~~ /< pre in post > 'fix:<' (<-[>]>+) '>'/;
-                        %trait{$name} = ~$0;
+                        %precedence{$name} = ~$0;
                         die X::Precedence::Incompatible.new
-                            if $identname ~~ /^ < pre post >/ && $s ~~ /^ in/
-                            || $identname ~~ /^ in/ && $s ~~ /^ < pre post >/;
+                            if $type eq ('prefix' | 'postfix') && $s ~~ /^ in/
+                            || $type eq 'infix' && $s ~~ /^ < pre post >/;
                     }($identifier.name);
                 }
                 elsif $name eq "assoc" {
@@ -489,69 +452,13 @@ class Parser {
                 }
             }
 
-            if %trait.keys > 1 {    # this might change in the future, when we have other traits
-                my ($t1, $t2) = %trait.keys.sort;
+            if %precedence.keys > 1 {
+                my ($t1, $t2) = %precedence.keys.sort;
                 die X::Trait::Conflict.new(:$t1, :$t2)
-                    if %trait{$t1} && %trait{$t2};
+                    if %precedence{$t1} && %precedence{$t2};
             }
 
-            if $identname ~~ /'infix:<' (<-[>]>+) '>'/ {
-                my $op = ~$0;
-                if %trait<looser> {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-infix-looser($op, Q::Infix::Custom["$op"], %trait<looser>, :$assoc);
-                }
-                elsif %trait<tighter> {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-infix-tighter($op, Q::Infix::Custom["$op"], %trait<tighter>, :$assoc);
-                }
-                elsif %trait<equal> {
-                    # we leave the associativity unspecified
-                    $*parser.oplevel.add-infix-equal($op, Q::Infix::Custom["$op"], %trait<equal>, :$assoc);
-                }
-                else {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-infix($op, Q::Infix::Custom["$op"], :$assoc);
-                }
-            }
-            elsif $identname ~~ /'prefix:<' (<-[>]>+) '>'/ {
-                my $op = ~$0;
-                if %trait<looser> {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-prefix-looser($op, Q::Prefix::Custom["$op"], %trait<looser>, :$assoc);
-                }
-                elsif %trait<tighter> {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-prefix-tighter($op, Q::Prefix::Custom["$op"], %trait<tighter>, :$assoc);
-                }
-                elsif %trait<equal> {
-                    # we leave the associativity unspecified
-                    $*parser.oplevel.add-prefix-equal($op, Q::Prefix::Custom["$op"], %trait<equal>, :$assoc);
-                }
-                else {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-prefix($op, Q::Prefix::Custom["$op"], :$assoc);
-                }
-            }
-            elsif $identname ~~ /'postfix:<' (<-[>]>+) '>'/ {
-                my $op = ~$0;
-                if %trait<looser> {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-postfix-looser($op, Q::Postfix::Custom["$op"], %trait<looser>, :$assoc);
-                }
-                elsif %trait<tighter> {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-postfix-tighter($op, Q::Postfix::Custom["$op"], %trait<tighter>, :$assoc);
-                }
-                elsif %trait<equal> {
-                    # we leave the associativity unspecified
-                    $*parser.oplevel.add-postfix-equal($op, Q::Postfix::Custom["$op"], %trait<equal>, :$assoc);
-                }
-                else {
-                    $assoc //= "left";
-                    $*parser.oplevel.add-postfix($op, Q::Postfix::Custom["$op"], :$assoc);
-                }
-            }
+            $*parser.oplevel.install($type, $op, :%precedence, :$assoc);
         }
 
         method statement:sub ($/) {
