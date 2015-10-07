e4979a5a079e841504b677a7d84a89e85a8f341f
Carl Masak <cmasak@gmail.com>
2015-10-07 21:47:20 +0200

    fulfill all the wishes on setting-wishlist
    (Thus also deleting setting-wishlist.)
    
    We've accrued a bit of a testing deficit by doing this.
    Also a deficit in typechecking. Hoping to remedy those two
    deficits as we get deeper into synthetic Qtrees.
    
    Some of these are funny. For example, `int` and `str` do double
    duty as coercers of actual in-program values, and as accessors/
    destructors of Qtree nodes (representing the int/str literals).

--- lib/_007/Runtime.pm
+++ lib/_007/Runtime.pm
@@ -117,8 +117,6 @@ role Runtime {
             max      => -> $a, $b { Val::Int.new(:value(max($a.value, $b.value))) },
             chr      => -> $arg { Val::Str.new(:value($arg.value.chr)) },
             ord      => -> $arg { Val::Int.new(:value($arg.value.ord)) },
-            int      => sub ($arg) { return Val::Int.new(:value($arg.value.Int)) if $arg.value ~~ /^ '-'? \d+ $/; return $arg },
-            str      => -> $arg { Val::Str.new(:value($arg.value.Str)) },
             chars    => -> $arg { Val::Int.new(:value($arg.value.Str.chars)) },
             uc       => -> $arg { Val::Str.new(:value($arg.value.uc)) },
             lc       => -> $arg { Val::Str.new(:value($arg.value.lc)) },
@@ -145,12 +143,125 @@ role Runtime {
                 }
                 $array;
             },
-            'Q::Postfix::Call' => -> $expr, $arguments { Q::Postfix::Call.new($expr, $arguments) },
+            'infix:<+>' => -> $lhs, $rhs { #`[not implemented here] },
+            'prefix:<->' => -> $lhs, $rhs { #`[not implemented here] },
+
+            'Q::Literal::Int' => -> $int { Q::Literal::Int.new($int.value) },
             'Q::Literal::Str' => -> $str { Q::Literal::Str.new($str.value) },
+            'Q::Literal::Array' => -> $array { Q::Literal::Array.new($array.value) },
+            'Q::Literal::Block' => -> $params, $stmts { Q::Literal::Block.new($params, $stmts) },
+            'Q::Literal::None' => -> { Q::Literal::None.new },
             'Q::Identifier' => -> $name { Q::Identifier.new($name.value) },
-            'Q::Arguments' => -> $arguments { Q::Arguments.new($arguments.elements) },
-            'infix:<+>' => -> $l, $r { #`[not implemented here] },
-            'prefix:<->' => -> $l, $r { #`[not implemented here] },
+            'Q::Statements' => -> $array { Q::Statements.new($array.value) },
+            'Q::Parameters' => -> $array { Q::Parameters.new($array.value) },
+            'Q::Arguments' => -> $array { Q::Arguments.new($array.elements) },
+            'Q::Prefix::Minus' => -> $expr { Q::Prefix::Minus.new($expr) },
+            'Q::Infix::Addition' => -> $lhs, $rhs { Q::Infix::Addition.new($lhs, $rhs) },
+            'Q::Infix::Concat' => -> $lhs, $rhs { Q::Infix::Concat.new($lhs, $rhs) },
+            'Q::Infix::Assignment' => -> $lhs, $rhs { Q::Infix::Assignment.new($lhs, $rhs) },
+            'Q::Infix::Eq' => -> $lhs, $rhs { Q::Infix::Eq.new($lhs, $rhs) },
+            'Q::Postfix::Call' => -> $expr, $args { Q::Postfix::Call.new($expr, $args) },
+            'Q::Postfix::Index' => -> $expr, $pos { Q::Postfix::Index.new($expr, $pos) },
+            'Q::Statement::My' => -> $ident, $assign = Empty { Q::Statement::My.new($ident, |$assign) },
+            'Q::Statement::Constant' => -> $ident, $assign { Q::Statement::Constant.new($ident, $assign) },
+            'Q::Statement::Expr' => -> $expr { Q::Statement::Expr.new($expr) },
+            'Q::Statement::If' => -> $expr, $block { Q::Statement::If.new($expr, $block) },
+            'Q::Statement::Block' => -> $block { Q::Statement::Block.new($block) },
+            'Q::Statement::Sub' => -> $ident, $params, $block { Q::Statement::Sub.new($ident, $params, $block) },
+            'Q::Statement::Macro' => -> $ident, $params, $block { Q::Statement::Macro.new($ident, $params, $block) },
+            'Q::Statement::Return' => -> $expr = Empty { Q::Statement::Return.new(|$expr) },
+            'Q::Statement::For' => -> $expr, $block { Q::Statement::For.new($expr, $block) },
+            'Q::Statement::While' => -> $expr, $block { Q::Statement::While.new($expr, $block) },
+            'Q::Statement::BEGIN' => -> $block { Q::Statement::BEGIN.new($block) },
+
+            int => sub ($_) {
+                when Val::Str {
+                    return Val::Int.new(:value(.value.Int))
+                        if .value ~~ /^ '-'? \d+ $/;
+                    proceed;
+                }
+                when Val::Int {
+                    return $_;
+                }
+                when Q::Literal::Int {
+                    return .eval(self);
+                }
+                die X::TypeCheck.new(
+                    :operation<int()>,
+                    :got($_),
+                    :expected("something that can be converted to an int"));
+            },
+            str => sub ($_) {
+                when Val { return Val::Str.new(:value(.value.Str)) }
+                when Q::Literal::Str {
+                    return .eval(self);
+                }
+                die X::TypeCheck.new(
+                    :operation<str()>,
+                    :got($_),
+                    :expected("something that can be converted to a string"));
+            },
+            array => sub ($_) {
+                when Val::Array {
+                    return $_;
+                }
+                when Q::Literal::Array {
+                    return .eval(self);
+                }
+                when Q::Statements {
+                    return Val::Array.new(:elements(.statements));
+                }
+                when Q::Parameters {
+                    return Val::Array.new(:elements(.parameters));
+                }
+                when Q::Arguments {
+                    return Val::Array.new(:elements(.arguments));
+                }
+                die X::TypeCheck.new(
+                    :operation<str()>,
+                    :got($_),
+                    :expected("something that can be converted to an array"));
+            },
+            params => sub ($_) {
+                # XXX: typecheck
+                return .parameters;
+            },
+            stmts => sub ($_) {
+                # XXX: typecheck
+                return .statements;
+            },
+            expr => sub ($_) {
+                # XXX: typecheck
+                return .expr;
+            },
+            lhs => sub ($_) {
+                # XXX: typecheck
+                return .lhs;
+            },
+            rhs => sub ($_) {
+                # XXX: typecheck
+                return .rhs;
+            },
+            pos => sub ($_) {
+                # XXX: typecheck
+                return .index;
+            },
+            args => sub ($_) {
+                # XXX: typecheck
+                return .arguments;
+            },
+            ident => sub ($_) {
+                # XXX: typecheck
+                return .ident;
+            },
+            assign => sub ($_) {
+                # XXX: typecheck
+                return .assignment;
+            },
+            block => sub ($_) {
+                # XXX: typecheck
+                return .block;
+            },
         ;
 
         for %builtins.kv -> $name, $sub {
