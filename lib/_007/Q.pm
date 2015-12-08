masak/alma

commit 23cf2aa94e384f9dfa7d95789b1a78f43366086f

remove helper sub 'pretty'
This sub was vestigial now that everything runs on Val:: objects.

lib/_007/Q.pm
@@ -38,19 +38,14 @@ class X::PropertyNotFound is Exception {
 
 role Q {
     method Str {
-        sub pretty($_) {
-            when Val::Array { return .quoted-Str }
-            when Val::Str { return .quoted-Str }
-            default { return .Str }
-        }
         sub aname($attr) { $attr.name.substr(2) }
         sub avalue($attr) { $attr.get_value(self) }
 
         my @attrs = self.attributes;
         if @attrs == 1 {
-            return "{self.^name} {pretty(avalue(@attrs[0]))}";
+            return "{self.^name} { avalue(@attrs[0]).Str }";
         }
-        sub keyvalue($attr) { aname($attr) ~ ": " ~ pretty(avalue($attr)) }
+        sub keyvalue($attr) { aname($attr) ~ ": " ~ avalue($attr) }
         if @attrs == 2 && aname(@attrs[1]) eq "expr" {  # prefix or postfix
             @attrs .= reverse;  # because it looks nicer to have expr first
         }

commit c66d522f7e6d8fdaf1e36577fd1c5b7d955830d4

restore quoting/escaping in Q stringification
Some previous commit, probably 23cf2aa9, accidentally removed
this. Restoring it.

We don't have any test coverage for the exact nature of Q
stringification. At least not for quoting/escaping. Maybe we
should have.

lib/_007/Q.pm
@@ -44,11 +44,11 @@ class X::Associativity::Conflict is Exception {
 role Q {
     method Str {
         sub aname($attr) { $attr.name.substr(2) }
-        sub avalue($attr) { $attr.get_value(self) }
+        sub avalue($attr) { $attr.get_value(self).quoted-Str }
 
         my @attrs = self.attributes;
         if @attrs == 1 {
-            return "{self.^name} { avalue(@attrs[0]).Str }";
+            return "{self.^name} { avalue(@attrs[0]) }";
         }
         sub keyvalue($attr) { aname($attr) ~ ": " ~ avalue($attr) }
         my $contents = @attrs.map(&keyvalue).join(",\n").indent(4);