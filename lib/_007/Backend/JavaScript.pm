eab3ccb7bc21469a5a02da07d2db2a8f4262a6b2
Carl Masak <cmasak@gmail.com>
2016-08-20 14:53:04 +0200

    Handle if statements in JS backend

--- lib/_007/Backend/JavaScript.pm
+++ lib/_007/Backend/JavaScript.pm
@@ -16,34 +16,51 @@ class _007::Backend::JavaScript {
         my @builtins;
         my @main;
 
-        for $compunit.block.statementlist.statements.elements -> $statement {
-            when $statement ~~ Q::Statement::Expr {
-                my $expr = $statement.expr;
-                when $expr ~~ Q::Postfix::Call
-                    && $expr.operand ~~ Q::Identifier
-                    && $expr.operand.name.value eq "say" {
-
-                    @builtins.push(%builtins<say>);
-                    my @arguments = $expr.argumentlist.arguments.elements.map: {
-                        die "Cannot handle non-literal-Str arguments just yet!"
-                            unless $_ ~~ Q::Literal::Str;
-                        .value.quoted-Str;
-                    };
-                    @main.push("say({@arguments.join(", ")});");
-                }
-
-                die "Cannot handle this type of Q::Statement::Expr yet!";
-            }
-
-            die "Cannot handle {$statement.^name} yet";
+        for $compunit.block.statementlist.statements.elements -> $stmt {
+            emit-stmt($stmt);
         }
 
         my $builtins = @builtins.map({ "$_\n" }).join;
         my $main = @main.join("\n");
         return qq:to 'PROGRAM';
             {$builtins}(() => \{ // main program
-                {$main}
+            {$main.indent(4)}
             \})();
             PROGRAM
+
+        multi emit-stmt(Q::Statement $stmt) {
+            die "Cannot handle {$stmt.^name}";
+        }
+
+        multi emit-stmt(Q::Statement::Expr $stmt) {
+            my $expr = $stmt.expr;
+            when $expr ~~ Q::Postfix::Call
+                && $expr.operand ~~ Q::Identifier
+                && $expr.operand.name.value eq "say" {
+
+                @builtins.push(%builtins<say>);
+                my @arguments = $expr.argumentlist.arguments.elements.map: {
+                    die "Cannot handle non-literal-Str arguments just yet!"
+                        unless $_ ~~ Q::Literal::Str;
+                    .value.quoted-Str;
+                };
+                @main.push("say({@arguments.join(", ")});");
+            }
+
+            die "Cannot handle this type of Q::Statement::Expr yet!";
+        }
+
+        multi emit-stmt(Q::Statement::My $stmt) {
+            my $name = $stmt.identifier.name.value;
+            if $stmt.expr !=== NONE {
+                die "Cannot handle non-literal-Int rhs just yet!"
+                        unless $stmt.expr ~~ Q::Literal::Int;
+                my $expr = $stmt.expr.value.Str;
+                @main.push("let {$name} = {$expr};");
+            }
+            else {
+                @main.push("let {$name};");
+            }
+        }
     }
 }
--- t/backend/javascript.t
+++ t/backend/javascript.t
@@ -12,4 +12,12 @@ emits-js q:to '====', ["say"], q:to '----', "hello world";
     say("Hello, world!");
     ----
 
+emits-js q:to '====', [], q:to '----', "'my' statement";
+    my foo;
+    my bar = 7;
+    ====
+    let foo;
+    let bar = 7;
+    ----
+
 done-testing;
