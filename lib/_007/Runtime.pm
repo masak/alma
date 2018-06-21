c17ec2037cb8fd7358d382b99843b19b6eec6598
Carl Masak <cmasak@gmail.com>
2018-06-21 11:34:39 +0200

    Provide hotlines to say/prompt
    As we load the builtins, also take references to the `say` and
    `prompt` stubs, so that we don't have to dig into pads to compare
    them in call(). This might very well be a premature optimization,
    but I also like the added clarity, I think.

--- lib/_007/Runtime.pm
+++ lib/_007/Runtime.pm
@@ -13,6 +13,8 @@ class _007::Runtime {
     has @!frames;
     has $.builtin-opscope;
     has $.builtin-frame;
+    has $!say-builtin;
+    has $!prompt-builtin;
 
     submethod BUILD(:$!input, :$!output) {
         self.enter(NO_OUTER, Val::Object.new, Q::StatementList.new);
@@ -143,6 +145,9 @@ class _007::Runtime {
                 :frame(NONE));
             self.declare-var($identifier, $value);
         }
+        my %builtins = %(builtins());
+        $!say-builtin = %builtins<say>;
+        $!prompt-builtin = %builtins<prompt>;
         $!builtin-opscope = opscope();
     }
 
@@ -151,11 +156,11 @@ class _007::Runtime {
         my $argcount = @arguments.elems;
         die X::ParameterMismatch.new(:type<Sub>, :$paramcount, :$argcount)
             unless $paramcount == $argcount;
-        if $c === $!builtin-frame.properties<pad>.properties<say> {
+        if $c === $!say-builtin {
             $.output.print(@arguments[0].Str ~ "\n");
             return NONE;
         }
-        if $c === $!builtin-frame.properties<pad>.properties<prompt> {
+        if $c === $!prompt-builtin {
             $.output.print(@arguments[0].Str);
             $.output.flush();
             return Val::Str.new(:value($.input.get()));
