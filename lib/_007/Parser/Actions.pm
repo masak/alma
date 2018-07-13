2de71d775a33a0e96ee7f54dac31e33ae150da9f
Carl Masak <cmasak@gmail.com>
2018-07-13 22:38:41 +0200

    Allow custom ops to be used inside themselves
    Somehow this hasn't worked all this time.

--- lib/_007/Parser/Actions.pm
+++ lib/_007/Parser/Actions.pm
@@ -11,24 +11,10 @@ class X::PointyBlock::SinkContext is Exception {
     method message { "Pointy blocks cannot occur on the statement level" }
 }
 
-class X::Trait::Conflict is Exception {
-    has Str $.trait1;
-    has Str $.trait2;
-
-    method message { "Traits '$.trait1' and '$.trait2' cannot coexist on the same routine" }
-}
-
 class X::Trait::Duplicate is Exception {
     has Str $.trait;
 
     method message { "Trait '$.trait' is used more than once" }
-}
-
-class X::Trait::IllegalValue is Exception {
-    has Str $.trait;
-    has Str $.value;
-
-    method message { "The value '$.value' is not compatible with the trait '$.trait'" }
 }
 
 class X::Macro::Postdeclared is Exception {
@@ -46,10 +32,6 @@ class X::Op::Nonassociative is Exception {
         my $name2 = $.op2.type.substr(1, *-1);
         "'$name1' and '$name2' do not associate -- please use parentheses"
     }
-}
-
-class X::Precedence::Incompatible is Exception {
-    method message { "Trying to relate a pre/postfix operator with an infix operator" }
 }
 
 class X::Property::NotDeclared is Exception {
